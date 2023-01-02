d7ea9a2d9696f367c2651677a95a7b863698fed2
Jose Camarena <jose.j.camarena@gmail.com>
2023-01-02 10:14:40 -0800

    commit all

--- src/main/java/com/mytest/web/repositories/MemoryRepository.java
+++ src/main/java/com/mytest/web/repositories/MemoryRepository.java
@@ -3,5 +3,9 @@ package com.mytest.web.repositories;
 import com.mytest.web.domain.Memory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
+    public List<Memory> findAllByDateEntryAfter(LocalDate dateEntry);
 }
--- src/main/java/com/mytest/web/services/MemoryService.java
+++ src/main/java/com/mytest/web/services/MemoryService.java
@@ -1,2 +1,32 @@
-package com.mytest.web.services;public class MemoryService {
+package com.mytest.web.services;
+
+import com.mytest.web.domain.Memory;
+import com.mytest.web.repositories.MemoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemoryService {
+    private MemoryRepository memoryRepository;
+    public MemoryService(MemoryRepository memoryRepository){
+        this.memoryRepository = memoryRepository;
+    }
+    public Optional<Memory> findById(Long id){
+        return memoryRepository.findById(id);
+    }
+    public Memory saveMemory(Memory memory){
+        return memoryRepository.save(memory);
+    }
+    public void deleteMemory(Memory memory){
+        memoryRepository.delete(memory);
+    }
+    public List<Memory> findAllByEntryDateAfterInput(LocalDate date){
+        return memoryRepository.findAllByDateEntryAfter(date);
+    }
+    public List<Memory> findAll(){
+        return memoryRepository.findAll();
+    }
 }
